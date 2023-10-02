0e78a4736d621028632e07f97f3af7acba567f80
Luiz Flavius Veras <luizflavius2@gmail.com>
2023-10-02 17:21:35 -0300

    Revert "1 - luizflavius - Created Initial Django Apps"
    This reverts commit 8046cf95ca4af7c94b6ed5908e7ca5640bd36540.

--- healthy/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('helloworld/', views.helloworld),
-    path('', views.homePage, name='home-page'),
-    path('yourname/<str:name>', views.yourName, name='your-name')
-]
--- setup/settings.py
+++ setup/settings.py
@@ -40,10 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'healthy',
-    'users',
-    'about',
-
 ]
 
 MIDDLEWARE = [
