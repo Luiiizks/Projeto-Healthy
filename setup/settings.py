34122f382ac7a1f7c9184984869993f252db6cca
Luiz Flavius Veras <luizflavius2@gmail.com>
2023-10-02 20:22:55 -0300

    2 - luizflavius - Login , Logout
    Features healthy app:
    Add:
    - Login
    - Logout

--- setup/settings.py
+++ setup/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'healthy',
     'users',
     'about',
+    'crispy_forms',
+    'crispy_bootstrap4',
 
 ]
 
@@ -61,7 +63,7 @@ ROOT_URLCONF = 'setup.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,9 +124,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = '/'
+
+LOGOUT_REDIRECT_URL = '/accounts/login'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
