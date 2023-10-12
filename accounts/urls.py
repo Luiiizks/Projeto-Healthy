bd76a557a0595d4c0bd4eaa98aa3b516f9b7e4ca
Maria Luísa <marialuisacoimbra@gmail.com>
2023-10-12 00:19:33 -0300

    0 | ChangeHomePage

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
 
 from . import views
 
 urlpatterns = [
     path('register/', views.SignUp.as_view(), name="signup"),
+    path('', LoginView.as_view(), name='login'),
+    path('home/', login_required(LoginView.as_view(), redirect_field_name=None), name='login'),
+    
 ]
--- setup/urls.py
+++ setup/urls.py
@@ -3,10 +3,12 @@ from django.urls import path , include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('healthy.urls')),
+    path('home/', include('healthy.urls')),
     path('goals/', include('goals.urls')),
+    path('', include('accounts.urls')),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
