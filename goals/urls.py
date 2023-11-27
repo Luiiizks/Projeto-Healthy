790f1b83012e5b8b35aa7a2eac37e300fec5288a
Luiz Flavius Veras <luizflavius2@gmail.com>
2023-11-26 23:18:29 -0300

    Add meta de gordura, Remove Generic
    removido generic views de accounts
    adicionado historia para adicionar meta de gordura

--- goals/urls.py
+++ goals/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("water_goal/", views.water_goal, name="water_goal"),
     path("weight_goal/", views.weight_goal, name="weight_goal"),
+    path("fat_percentage_goal/", views.fat_percentage_goal, name="fat_percentage_goal"), 
 
     path("", views.goals, name="goal"),
 ]
