d297ffa7c79bfae5e96195d0c63b7df35506e7f9
mihlamonteiro <mirnalustosa2003@gmail.com>
2023-11-28 00:05:40 -0300

    requirements

--- testes/cadastro-teste.py
+++ testes/cadastro-teste.py
@@ -22,7 +22,7 @@ wait = WebDriverWait(driver, 10)
 username_input = wait.until(EC.visibility_of_element_located((By.NAME, 'username')))
 
 # Insira algum texto no campo de entrada
-username_input.send_keys("daniloevirolivss")
+username_input.send_keys("daniloevirolivsss")
 
 # Aguarde alguns segundos para visualizar o resultado (opcional)
 driver.implicitly_wait(5)
@@ -56,7 +56,7 @@ wait = WebDriverWait(driver, 10)
 username_input = wait.until(EC.visibility_of_element_located((By.NAME, 'username')))
 
 # Insira algum texto no campo de entrada
-username_input.send_keys("daniloevirolivss")
+username_input.send_keys("daniloevirolivsss")
 
 # Aguarde alguns segundos para visualizar o resultado (opcional)
 driver.implicitly_wait(5)
